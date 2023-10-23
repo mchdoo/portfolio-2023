fe860eae6e98fbdf2625841a990fd5e7e567bab6
Pedro Machado <pepimach123@gmail.com>
2023-10-23 15:21:53 -0300

    final strike

--- src/components/renderImage.tsx
+++ src/components/renderImage.tsx
@@ -1,13 +1,23 @@
 import { TypeRender } from "@/lib/types";
 import type { AssetFields, Asset } from "contentful";
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
 
-export default function RenderImage({render}: {render: Asset<undefined, string>}) {
-    const {title, file} = render.fields;
-    const url = 'https:' + file.url;
+export default function RenderImage({
+  render,
+}: {
+  render: Asset<undefined, string>;
+}) {
+  const { title, file } = render.fields;
+  const url = "https:" + file!.url;
 
-    return (
-        <Image className="w-full h-full max-h-[300px] object-cover object-center"  alt={file.fileName} src={url} height={file.details.image.height / 5} width={file.details.image.width / 5} />
-        )
+  return (
+    <Image
+      className="w-full h-full max-h-[300px] object-cover object-center"
+      alt={file!.fileName}
+      src={url}
+      height={file!.details.image!.height / 5}
+      width={file!.details.image!.width / 5}
+    />
+  );
 }
